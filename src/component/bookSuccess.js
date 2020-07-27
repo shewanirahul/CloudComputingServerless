6b2ce8ad43745b5a09626ebe96c64a79e8d5b26a
shewani <shewanirahul@dal.ca>
2020-07-27 19:00:46 -0300

    Added Serverless changes to Z company frontend

---
+++ src/component/bookSuccess.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class bookSuccess extends React.Component {
+    render() {
+        return(
+            <div>
+                <h3>Successfully Booked your Job order!</h3>
+                <br></br>
+                <br></br>
+                <br></br>
+                <a href="/">Home Page</a>
+            </div>
+        );
+      }
+}
+
+export default bookSuccess;
